fd55150248fefae3b90a4719cd1c266d8dabb9b9
Air <airzhaoyn@gmail.com>
2019-09-05 14:25:45 +0800

    use kotlin

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,53 +4,61 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
-            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+            when {
+                items[i].name == "Aged Brie" -> increaseQuality(i)
+                items[i].name == "Backstage passes to a TAFKAL80ETC concert" -> handleBackstagePasses(i)
+                else -> decreaseQuality(i)
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
                 items[i].sellIn = items[i].sellIn - 1
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
+            if (items[i].sellIn >= 0) {
+                continue
+            }
+
+            if (items[i].name == "Aged Brie") {
+                increaseQuality(i)
+            } else {
+                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                    items[i].quality = 0
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
+                    decreaseQuality(i)
                 }
             }
+        }
+    }
+
+
+    private fun increaseQuality(i: Int) {
+        if (items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1
+        }
+    }
+
+    private fun decreaseQuality(index: Int) {
+        if (items[index].quality <= 0) {
+            return
+        }
+
+        if (items[index].name != "Sulfuras, Hand of Ragnaros") {
+            items[index].quality = items[index].quality - 1
+        }
+    }
+
+    private fun handleBackstagePasses(index: Int) {
+        if (items[index].quality >= 50) {
+            return
+        }
+        increaseQuality(index)
+
+        if (items[index].sellIn < 11) {
+            increaseQuality(index)
+        }
+
+        if (items[index].sellIn < 6) {
+            increaseQuality(index)
         }
     }
 
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,18 +1,126 @@
 package com.gildedrose
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class GildedRoseTest {
 
-    @Test fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
+    @Test
+    fun foo() {
+        val items = arrayOf(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
-
+        assertEquals("foo", app.items[0].name)
     }
 
+    @Test
+    fun lower() {
+        val items = arrayOf(Item("foo", 1, 2))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(0, app.items[0].sellIn)
+        assertEquals(1, app.items[0].quality)
+    }
+
+    @Test
+    fun qualityDegradesTwice() {
+        val items = arrayOf(Item("foo", 1, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        app.updateQuality()
+        assertEquals(-1, app.items[0].sellIn)
+        assertEquals(7, app.items[0].quality)
+    }
+
+    @Test
+    fun neverNegativeQuality() {
+        val items = arrayOf(Item("foo", 1, 1))
+        val app = GildedRose(items)
+        app.updateQuality()
+        app.updateQuality()
+        assertEquals(-1, app.items[0].sellIn)
+        assertEquals(0, app.items[0].quality)
+    }
+
+    @Test
+    fun testIncreaseQualityOfAgedBrie() {
+        val items = arrayOf(Item("Aged Brie", 1, 1))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(0, app.items[0].sellIn)
+        assertEquals(2, app.items[0].quality)
+    }
+
+    @Test
+    fun testQualityNotMoreThan50() {
+        // 假设item初始值不允许 > 50
+        val items = arrayOf(Item("Aged Brie", 3, 49))
+        val app = GildedRose(items)
+        app.updateQuality()
+        app.updateQuality()
+        assertEquals(1, app.items[0].sellIn)
+        assertEquals(50, app.items[0].quality)
+    }
+
+    @Test
+    fun legendarySulfuras() {
+        val items = arrayOf(Item("Sulfuras, Hand of Ragnaros", 1, 2))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(1, app.items[0].sellIn)
+        assertEquals(2, app.items[0].quality)
+        app.updateQuality()
+        assertEquals(1, app.items[0].sellIn)
+        assertEquals(2, app.items[0].quality)
+    }
+
+    @Test
+    fun testIncreaseQualityBackstagePasses() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 30, 2))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(29, app.items[0].sellIn)
+        assertEquals(3, app.items[0].quality)
+        app.updateQuality()
+        assertEquals(28, app.items[0].sellIn)
+        assertEquals(4, app.items[0].quality)
+    }
+
+    @Test
+    fun testIncreaseQualityFrom6To10BackstagePasses() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 2))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(9, app.items[0].sellIn)
+        assertEquals(4, app.items[0].quality)
+        app.updateQuality()
+        assertEquals(8, app.items[0].sellIn)
+        assertEquals(6, app.items[0].quality)
+    }
+
+    @Test
+    fun testIncreaseQualityFrom1To5BackstagePasses() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 5, 2))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(4, app.items[0].sellIn)
+        assertEquals(5, app.items[0].quality)
+        app.updateQuality()
+        assertEquals(3, app.items[0].sellIn)
+        assertEquals(8, app.items[0].quality)
+    }
+
+    @Test
+    fun testIncreaseQualityFrom0To1BackstagePasses() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 1, 2))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(0, app.items[0].sellIn)
+        assertEquals(5, app.items[0].quality)
+        app.updateQuality()
+        assertEquals(-1, app.items[0].sellIn)
+        assertEquals(0, app.items[0].quality)
+    }
 
 }
 
